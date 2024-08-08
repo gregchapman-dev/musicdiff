1f8ff1129ced9feed175c9c126d6adb437bfc1d8
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-08-08 12:43:50 -0700

    Prep for v4.0.0 release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages
 import pathlib
 
-musicdiffversion = '3.1.1'
+musicdiffversion = '4.0.0'
 
 here = pathlib.Path(__file__).parent.resolve()
 
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         install_requires=[
             'music21>=9.1',
             'numpy',
-            'converter21>=3.1'
+            'converter21>=3.2'
         ],
 
         project_urls={
